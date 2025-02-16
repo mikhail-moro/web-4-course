ae36ea1a2c76ea63c4ed4e6e9380820a174d0cab
IlonMaxi <108355205+IlonMaxi@users.noreply.github.com>
2025-02-16 17:57:02 +0300

    first settings

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>Привет, Vue.js в Laravel!</h1>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  font-family: Arial, sans-serif;
+  font-size: 24px;
+  color: #ffffff;
+  background-color: #282c34;
+}
+</style>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel + Vue</title>
+    @vite(['resources/js/app.js']) <!-- Подключаем Vite -->
+</head>
+<body>
+    <div id="app"></div> <!-- Контейнер Vue -->
+</body>
+</html>
