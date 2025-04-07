13f7adc8b66a40e4ccf37b6fba7a9feb20ef64ea
mikhail.morozov <mikhail.morozov@softezza.ru>
2025-04-07 16:55:59 +0300

    CI-CD changes

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import ip from 'ip';
 
-//const host= ip.address().startsWith("172.") ? "194.87.238.106" : "localhost";
+const host= ip.address().startsWith("172.") ? "194.87.238.106" : "localhost";
 
 export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'localhost',
-            port: 81,  // Порт Nginx
+            host: host,
+            port: 80,  // Порт Nginx
             protocol: 'ws',  // WebSocket
         },
     },
